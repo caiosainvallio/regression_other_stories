b6599afa46bff552370b7f157731bbc925113851
Caio Sain Vallio <caiosainvallio@me.com>
2021-11-05 07:17:57 -0300

    Add files via upload

--- cap_13_logistic_regression.R
+++ cap_13_logistic_regression.R
@@ -9,9 +9,23 @@ URL <- "https://raw.githubusercontent.com/avehtari/ROS-Examples/master/NES/data/
 nes92 <- read.table(URL, header = TRUE)
 
 fit_1 <- stan_glm(rvote ~ income, family=binomial(link="logit"), data=nes92)
-print(fit_1)
+print(fit_1, digits = 3)
 
 plot(nes92$income, nes92$rvote)
 curve(invlogit(coef(fit_1)[1] + coef(fit_1)[2]*x), add=TRUE)
+
+
+# probabilidade usando a média de income
+invlogit(-0.687 + 0.232*mean(nes92$income))
+plogis(-0.687 + 0.232*mean(nes92$income))
+
+invlogit(coef(fit_1)[1] + coef(fit_1)[2]*mean(nes92$income))
+plogis(coef(fit_1)[1] + coef(fit_1)[2]*mean(nes92$income))
+
+# A difference of 1 in income (on this 1–5 scale) corresponds to a positive 
+# difference of 0.33 in the logit probability of supporting Bush.
+
+# log(odds) interpretation
+exp(0.232)
 
 
