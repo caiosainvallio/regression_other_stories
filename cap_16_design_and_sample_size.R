ff8df1a489a7fbcbd65000e6eb21fbd12f4aa273
Caio Sain Vallio <caiosainvallio@me.com>
2021-11-11 07:35:35 -0300

    Add files via upload

--- cap_16_design_and_sample_size.R
+++ cap_16_design_and_sample_size.R
@@ -24,3 +24,30 @@ n <- (2.8*1.15/0.10)^2
 n*.25 # in the int group
 n*.75 # in the cont group
 
+
+
+## The 2.8 ---------------------------------------------------------------------
+# In summary, to have 80% power, the true value of the parameter must be 2.8 
+# standard errors away from the comparison point: the value 2.8 is 1.96 from 
+# the 95% interval, plus 0.84 to reach the 80th percentile of the normal 
+# distribution.
+
+
+
+# Estimates of means -----------------------------------------------------------
+
+# Teh quick estimete of Theta is the sample mena, y_hat, wich has a sttandard
+# error of Sigma/sqrt(n), where Sigma is the standard deviation of y in the 
+# population. 
+
+# If the goal is to acheive a specific s.e. for y_hat, then the sample size must
+# be at least n = (Sigma/s.r.)^2.
+
+# If the goal is 89% power to distinguish Theta frm a specified value Theta_0,
+# then a conservative required sample size is n = (2.8Sigma/(Theta - Theta_0))^2.
+
+# *2.8 standard errors from zero.
+
+
+
+
